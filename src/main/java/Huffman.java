a8c2153c6fdd4d7502c5dbf1e6b8cfbdb1e78acc
daniel.barber <daniel.barber@students.fhnw.ch>
2023-12-03 15:31:48 +0100

    Huffman Baum implementieren

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -1,11 +1,50 @@
 package src.main.java;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 public class Huffman {
 
     public static void main(String[] args) {
-        System.out.print(FileReader.readTextFile("hallo-ipt"));
+        Map<Integer, Integer> frequencyTable = FileReader.readTextFile("hallo-ipt");
+        PriorityQueue<HuffmanNode> priorityQueue = new PriorityQueue<>();
+        System.out.println(frequencyTable);
 
+        //Blätter erzeugen
+        for (Map.Entry<Integer, Integer> entry : frequencyTable.entrySet()) {
+            HuffmanNode node = new HuffmanNode(entry.getKey(), entry.getValue());
+            priorityQueue.add(node);
+        }
+
+        //Baum erstellen
+        while (priorityQueue.size()>1){
+            HuffmanNode left = priorityQueue.poll();
+            HuffmanNode right = priorityQueue.poll();
+
+            HuffmanNode merged = new HuffmanNode(-1, left.frequency+ right.frequency);
+            merged.left = left;
+            merged.right = right;
+
+            priorityQueue.add(merged);
+        }
+
+        //Huffman Code implementieren
+        HuffmanNode root = priorityQueue.poll();
+        Map<Integer,String> huffmanCodes = new HashMap<>();
+        generateCodes(root, "", huffmanCodes);
+
+
+    }
+
+    private static void generateCodes(HuffmanNode node, String code, Map<Integer, String> huffmanCodes) {
+        if(node.character != -1){
+            huffmanCodes.put(node.character, code);
+            return;
+        }
+        generateCodes(node.left, code + "0", huffmanCodes);
+        generateCodes(node.right, code + "1", huffmanCodes);
     }
 }
---
+++ src/main/java/HuffmanNode.java
@@ -0,0 +1,18 @@
+package src.main.java;
+
+public class HuffmanNode implements Comparable<HuffmanNode> {
+    int character;
+    int frequency;
+    HuffmanNode left;
+    HuffmanNode right;
+
+    public HuffmanNode(int character, int frequency) {
+        this.character = character;
+        this.frequency = frequency;
+    }
+
+    @Override
+    public int compareTo(HuffmanNode node) {
+        return this.frequency - node.frequency;
+    }
+}
